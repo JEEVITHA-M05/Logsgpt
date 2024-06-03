bacb3e9de36d33c1109302b307a7451687ba2465
JEEVITHA-M05 <jeevitham439@gmail.com>
2024-06-03 14:17:57 +0530

    queries of Windows logs

---
+++ constant/data_constant/__init__.py
@@ -0,0 +1,6 @@
+ES_USER= "elastic"
+ES_PASSWORD= "YrZX7odLu=PWQzq_PPAk"
+ES_URL= "http://localhost:9200"
+LOGS_INDEX= "logs-generic-default"
+EMBEDDINGS_INDEX= "jwinlogs_embedding"
+
---
+++ pipeline.py
@@ -0,0 +1,19 @@
+from components.es_store import ElasticStore
+from utilities.config import DataConstants
+from components.logs_extraction import ElasticSearchProcessor  
+from components.embedding_model import EmbeddingModel  
+from langchain_elasticsearch import ElasticsearchStore
+from logger import logger
+
+class Pipelinee:
+    @staticmethod
+    def run_pipeline():
+        try:
+            data_constants=DataConstants()
+            store=ElasticStore()
+            index_name = data_constants.logs_index
+            interval = 40  # Time interval in seconds
+            timestamp_file = "last_processed_timestamp.txt"
+            ElasticStore.continuous_process(index_name, interval, timestamp_file)
+        except Exception as e:
+            logger.error(f"Error in pipeline execution: {e}")
